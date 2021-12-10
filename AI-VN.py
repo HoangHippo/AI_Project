9a30519a189d79671974e36cdd3375eddae46043
Alex <alexnguyen123229@gmail.com>
2021-12-10 11:11:02 +0700

    weather

--- AI-VN.py
+++ AI-VN.py
@@ -6,7 +6,8 @@ from gtts import gTTS
 import datetime
 import webbrowser as wb
 import re
-
+import requests
+import json
 r = sr.Recognizer()
 
 
@@ -91,6 +92,56 @@ def open_web(text):
         return False
 
 
+def current_weather():
+    speak("Bạn muốn xem thời tiết ở đâu ạ.")
+    # Đường dẫn trang web để lấy dữ liệu về thời tiết
+    ow_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city = command()   # lưu tên thành phố vào biến city
+    if not city:    # nếu biến city != 0 và = False thì để đấy ko xử lí gì cả
+        pass
+    # api_key lấy trên open weather map
+    api_key = "540c4efbf623055574abfc7d8772cab4"
+    call_url = ow_url + "appid=" + api_key + "&q=" + city + \
+        "&units=metric"    # tìm kiếm thông tin thời thời tiết của thành phố
+    # truy cập đường dẫn lấy dữ liệu thời tiết
+    response = requests.get(call_url)  # gửi yêu cầu lấy dữ liệu
+    data = response.json()    # lưu dữ liệu thời tiết dưới dạng json và cho vào biến data
+    if data["cod"] != "404":     # kiểm tra nếu ko gặp lỗi 404 thì xem xét và lấy dữ liệu
+        # lấy dữ liệu của key main
+        city_res = data["main"]
+        # nhiệt độ hiện tại
+        current_temperature = city_res["temp"]
+        # áp suất hiện tại
+        current_pressure = city_res["pressure"]
+        # độ ẩm hiện tại
+        current_humidity = city_res["humidity"]
+        # thời gian mặt trời
+        suntime = data["sys"]
+        # 	lúc mặt trời mọc, mặt trời mọc
+        sunrise = datetime.datetime.fromtimestamp(suntime["sunrise"])
+        # lúc mặt trời lặn
+        sunset = datetime.datetime.fromtimestamp(suntime["sunset"])
+        # thông tin thêm
+        wthr = data["weather"]
+        # mô tả thời tiết
+        weather_description = wthr[0]["description"]
+        # Lấy thời gian hệ thống cho vào biến now
+        now = datetime.datetime.now()
+        # hiển thị thông tin với người dùng
+        content = f"""
+        Hôm nay là ngày {now.day} tháng {now.month} năm {now.year}
+        Mặt trời mọc vào {sunrise.hour} giờ {sunrise.minute} phút
+        Mặt trời lặn vào {sunset.hour} giờ {sunset.minute} phút
+        Nhiệt độ trung bình là {current_temperature} độ C
+        Áp suất không khí là {current_pressure} héc tơ Pascal
+        Độ ẩm là {current_humidity}%
+        """
+        speak(content)
+    else:
+        # nếu tên thành phố không đúng thì nó nói dòng dưới 227
+        speak("Không tìm thấy địa chỉ của bạn")
+
+
 if __name__ == "__main__":
     welcome()
 
@@ -131,6 +182,10 @@ if __name__ == "__main__":
 
         elif "mở" in text:
             open_app(text)
+
+        elif "thời tiết" in text:
+            current_weather()
+
 
 # Thêm chức năng ở trên đây
         elif "tạm biệt" in text:
